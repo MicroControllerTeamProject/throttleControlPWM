38a1adb1c19d00c549ad19bada27ff802f7dd013
Luigi Santagada <luigi.santagada@live.com>
2023-08-06 14:55:42 +0200

    .

---
+++ throttleControlPWM/throttleControlPWM.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		throttleControlPWM.ino
+ Created:	8/6/2023 2:48:49 PM
+ Author:	luigi.santagada
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
